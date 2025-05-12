bdcbfc5bd419567ba436a0eaddcb790985b33d85
Narumimin <diegoaaroncs8@gmail.com>
2025-05-12 08:20:51 -0600

    THINGS

--- Assets/Scripts/Enemies/LimboMan.cs
+++ Assets/Scripts/Enemies/LimboMan.cs
@@ -21,14 +21,19 @@ public class LimboMan : MonoBehaviour
             i++; // cuenta para arriba para pasar al siguiente punto
             if (i == points.Length) //compara cuantos por cuantos puntos ha pasado el objeto al numero de puntos totales
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                //transform.localScale = new Vector3(-1, 1, 1);
                 i = 0; //reinicia los puntos al inicio para que el objeto regrese a su lugar inicial
             }
             else
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                //transform.localScale = new Vector3(1, 1, 1);
             }
         }
+        Vector2 movement = transform.position - points[i].position;
+        if (movement.x < 0)
+           transform.localScale = new Vector3(1f, 1f, 1f);
+        if (movement.x > 0)
+           transform.localScale = new Vector3(-1f, 1f, 1f);
         transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime); //Mueve el onjeto hacia uno de los puntos donde va a pasar
     }
 
